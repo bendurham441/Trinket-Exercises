eaaccdcbc6632497d10023cda512e1907345338a
Ben Durham <bendurham441@gmail.com>
2017-09-13 21:06:15 -0400

    initial commit

---
+++ ch1Ex2.java
@@ -0,0 +1,7 @@
+public class ch1Ex2 {	// I know that this is not a good name, but it is just to let you know what it is, for now
+
+	public static void main (String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("How are you?"); 	// Prints "How are you?"
+	}
+}
