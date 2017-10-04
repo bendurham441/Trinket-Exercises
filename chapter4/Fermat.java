ad525ea48609d1836ce79b96d5added3b81e7582
Ben Durham <bendurham441@gmail.com>
2017-10-04 17:04:44 -0400

    Finished Fermat.java

--- chapter4/Fermat.java
+++ chapter4/Fermat.java
@@ -7,8 +7,11 @@
  */
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Fermat {
+
+	// Method to calculate whether fermats theory works on three given numbers and their exponents.
 	public static void checkFermat(int a, int b, int c, int n) {
 		int result = (int) Math.pow(a, n) + (int) Math.pow(b, n);
 		
@@ -18,24 +21,70 @@ public class Fermat {
 	}
 
 	public static void main(String[] args) {
+
+		// Opens a new scanner and initializes variables needed to recieve scanner input
 		Scanner kbd = new Scanner(System.in);
 		int a, b, c, n;
+	
+		// Get a value	
+		System.out.print("Enter an \"a\" value: ");
+		
+		while (true) {
+			try {
+				// Given the correct input, assigns variable and breaks from loop
+				a = kbd.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				// Catches the exception and loops back
+				System.out.print("Invalid input. Please re enter: ");
+				kbd.nextLine();
+			}
+		}
 
-		System.out.print("Enter in an \"a\" value: ");
-		if (kbd.hasNextInt()) a = kbd.nextInt();
-		else System.out.println("Please enter an integer");
+		// Get b value
+		System.out.print("Enter an \"b\" value: ");
+		
+		while (true) {
+			try {
+				// Given the correct input, assigns variable and breaks from loop
+				b = kbd.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				// Catches the exception and loops back
+				System.out.print("Invalid input. Please re enter: ");
+				kbd.nextLine();
+			}
+		}
+		
+		// Get c value
+		System.out.print("Enter an \"c\" value: ");
+		
+		while (true) {
+			try {
+				// Given the correct input, assigns variable and breaks from loop
+				c = kbd.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				// Catches the exception and loops back
+				System.out.print("Invalid input. Please re enter: ");
+				kbd.nextLine();
+			}
+		}
 
-		System.out.print("Enter in an \"b\" value: ");
-		if (kbd.hasNextInt()) b = kbd.nextInt();
-		else System.out.println("Please enter an integer");
-
-		System.out.print("Enter in an \"c\" value: ");
-		if (kbd.hasNextInt()) c = kbd.nextInt();
-		else System.out.println("Please enter an integer");
-
-		System.out.print("Enter in an \"n\" value: ");
-		if (kbd.hasNextInt()) n = kbd.nextInt();
-		else System.out.println("Please enter an integer");
+		// Get n value
+		System.out.print("Enter an \"n\" value: ");
+		
+		while (true) {
+			try {
+				// Given the correct input, assigns variable and breaks from loop
+				n = kbd.nextInt();
+				break;
+			} catch (InputMismatchException e) {
+				// Catches the exception and loops back
+				System.out.print("Invalid input. Please re enter: ");
+				kbd.nextLine();
+			}
+		}
 
 		checkFermat(a, b, c, n);
 	}
