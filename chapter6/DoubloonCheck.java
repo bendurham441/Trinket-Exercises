234190e4481443310a08349b97cc46922043a584
Ben Durham <bendurham441@gmail.com>
2017-10-27 07:47:30 -0400

    Added the right version of doubloon check

--- chapter6/DoubloonCheck.java
+++ chapter6/DoubloonCheck.java
@@ -1,25 +1,43 @@
 /**
- * DoubloonCheck.java
+ * DoubloonCheck.java 
  * @author Ben Durham
- * @version 20171025
- * Checks to see if a word is a doubloon
+ * @version 20171026
+ * Checks to see if a given string is a doubloon
+ * Chapter 6, Exercise 5
  */
-
-import java.util.Scanner;
-
+ 
+ import java.util.Scanner;
+ 
 public class DoubloonCheck {
-	public static boolean isDoubloon(String str) {
-		for (int i = 0; i < str.length(); i++) {
-			if (str.substring(i+1).contains(String.valueOf(str.charAt(i)))) {
-				if (str.substring(i+1).contains(String.valueOf(str.charAt(i)))) {
-					return false;
-				}
-			} else return false;
-		}
-		return true;
-	}
-		
-	public static void main(String[] args) {
-		System.out.println(isDoubloon("bboo"));
-	}
+  /**
+   * Checks to see if the word is a doubloon by looping through the letters of the word
+   * and finding matches. Returns if the number of pairs of letters is half of the
+   * length of the given word
+   * 
+   * @param str the string to check to see if it is a doubloon
+   * @return    whether not the given str is a doubloon
+   */ 
+  public static boolean isDoubloon(String str) {
+    str = str.toLowerCase();
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+      for (int i2 = i + 1; i2 < str.length(); i2++) {
+        if (str.charAt(i) == str.charAt(i2)) count++;
+      }
+    }
+    if (count == str.length() / 2.0) return true;
+    return false;
+  }
+  public static void main(String[] args) {
+    // Creating a new Scanner object
+    Scanner kbd = new Scanner(System.in);
+    
+    // Prompts the user for a word and stores it in input
+    System.out.println("Enter a word.");
+    String input = kbd.nextLine();
+    
+    // Checks to see if the word is a doubloon and prints a corresponding response
+    if (isDoubloon(input)) System.out.println("That is a doubloon.");
+    else System.out.println("That isn't a doubloon.");
+  }
 }
