45896b1658c6677753297ff83d434465b625b4c7
Ben Durham <bendurham441@gmail.com>
2017-11-28 15:05:14 +0000

    Finished Song.java

---
+++ chapter8/Song.java
@@ -0,0 +1,37 @@
+/**
+ * Song.java
+ * @author Ben Durham
+ * @version 20171128
+ * Prints the song "Bottles of Beer on the wall" using a recursive method
+ */
+
+public class Song {
+    /**
+     * Recursively prints the "bottles of beer" song
+     * 
+     * @param n     The number to start with in the song
+     */
+    public static void printSong(int n) {
+        if (n <= 0) {
+            // Breaks out of recursion once they reach 0
+            System.out.println("No bottles of beer on the wall,");
+            System.out.println("no bottles of beer,");
+            System.out.println("ya' can't take one down, ya' can't pass it around,");
+            System.out.println("'cause there are no more bottles of beer on the wall!");
+        } else {
+            // If n is not 0, prints out the song with the given number
+            System.out.printf("%d bottles of beer on the wall,\n", n);
+            System.out.printf("%d bottles of beer,\n", n);
+            System.out.printf("ya' take one down, ya' pass it around,\n");
+            System.out.printf("%d bottles of beer on the wall.\n", n - 1);
+            
+            // Calls the method again with the number 1 less than n
+            printSong(n - 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        // Calls the method, starting with 99
+        printSong(99);
+    }
+}
