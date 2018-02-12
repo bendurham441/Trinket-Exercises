d6e8f97b4149a4870144d3246559b70f7beabd98
Ben Durham <bendurham441@gmail.com>
2018-02-12 22:25:43 +0000

    Started War.java

--- chapter12/Card.java
+++ chapter12/Card.java
@@ -38,10 +38,10 @@ public class Card {
         if (this.suit > that.suit) {
             return 1;
         } 
-        if (this.rank < that.rank) {
+        if (that.rank == 1 || this.rank < that.rank) {
             return -1;
         }
-        if (this.rank > that.rank) {
+        if (this.rank == 1 || this.rank > that.rank) {
             return 1;
         }
         return 0;
@@ -52,9 +52,66 @@ public class Card {
         return s;
     }
     
+    public static int search(Card[] cards, Card target) {
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i].equals(target)) return i;
+        }
+        return -1;
+    }
+    
+    public static int binarySearch(Card[] cards, Card target) {
+        int low = 0;
+        int high = cards.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int comp = cards[mid].compareTo(target);
+            
+            if (comp == 0) {
+                return mid;
+            } else if (comp < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
     public static void printDeck(Card[] cards) {
         for (Card card: cards) {
             System.out.println(card);
         }
+    }
+    
+    public static Card[] makeDeck() {
+        Card[] cards = new Card[52];
+        int index = 0;
+        for (int suit = 0; suit < 4; suit++) {
+            for (int rank = 0; rank < 14; rank++) {
+                cards[index] = new Card(suit, rank);
+                index++;
+            }
+        }
+        return cards;
+    }
+    
+    public static void shuffle(Card[] cards) {
+        
+    }
+    
+    public static Card[] makeDeckOne(Card s) {
+        Card[] cards = new Card[52];
+        for (int i = 0; i < 52; i++) {
+            cards[i] = s;
+        }
+        return cards;
+    }
+    
+    public static int[] suitHist(Card[] cards) {
+        int[] hist = new int[4];
+        for (int i = 0; i < cards.length; i++) {
+            hist[cards[i].suit]++;
+        }
+        return hist;
     }
 }
---
+++ chapter12/War.java
@@ -0,0 +1,15 @@
+/**
+ * War.java
+ * @author Ben Durham
+ * @version 20180212
+ * War Card game
+ */
+
+public class War {
+    public static void main(String[] args) {
+        System.out.println("Welcome to war.");
+        Card[] player_cards = new Card[52];
+        Card[] cpu_cards = new Card[52];
+        
+    }
+}
